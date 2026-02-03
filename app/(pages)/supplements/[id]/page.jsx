25352a7a20490042fc48632c4ed16240932cb302
afterrender1 <afterrender1@gmail.com>
2026-02-03 19:25:06 +0500

    Update page.jsx

--- app/(pages)/supplements/[id]/page.jsx
+++ app/(pages)/supplements/[id]/page.jsx
@@ -393,9 +393,9 @@ export default async function ProductDetailPage({ params }) {
                             <Link
                                 key={item.id}
                                 href={`/supplements/${item.id}`}
-                                className="group bg-white rounded-2xl border border-slate-100 overflow-hidden  hover:-translate-y-1 transition-all duration-300"
+                                className="group bg-white rounded-2xl border border-slate-50 overflow-hidden  hover:-translate-y-1 transition-all duration-300"
                             >
-                                <div className="aspect-square p-6 flex items-center justify-center bg-slate-50/50 relative">
+                                <div className="aspect-square p-6 flex items-center justify-center  relative">
                                     <Image
                                         src={item.image_url}
                                         alt={item.name}
@@ -412,14 +412,14 @@ export default async function ProductDetailPage({ params }) {
                                         </span>
                                     )}
                                 </div>
-                                <div className="p-0 md:p-4">
+                                <div className="p-1 md:p-4">
                                     <p className="text-[0.6rem] md:text-[0.7rem] font-semibold uppercase tracking-wider text-slate-400 mb-1">
                                         {item.vendor}
                                     </p>
                                     <h3 className="font-semibold text-slate-900 text-[0.7rem] md:text-[0.9rem] line-clamp-2 mb-2 group-hover:text-slate-600 transition-colors">
                                         {item.name}
                                     </h3>
-                                    <div className="inline-flex items-center gap-4 px-2 py-2 bg-white border border-slate-200 rounded-lg  shadow-[0_2px_10px_-3px_rgba(0,0,0,0.07)]">
+                                    <div className="inline-flex items-center gap-4 px-2 py-2  shadow-[0_2px_10px_-3px_rgba(0,0,0,0.07)]">
                                         <div className="flex flex-col">
                                             <span className="text-[1.1rem] font-bold text-slate-800 tracking-tight">
                                                 ${item.sale_price.toLocaleString()}
