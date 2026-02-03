78151b0fa37841d2a0a731112163f15a3cd78a27
afterrender1 <afterrender1@gmail.com>
2026-02-03 19:49:38 +0500

    Update page.jsx

--- app/(pages)/shop/supplements/page.jsx
+++ app/(pages)/shop/supplements/page.jsx
@@ -10,21 +10,13 @@ const ShopPage = () => {
         <div className="relative  bg-black min-h-screen">
             <Navbar />
 
-            {/* STICKY WRAPPER 
-                h-[100vh] ensures the hero stays visible for the full viewport.
-                z-0 keeps it behind the upcoming content.
-            */}
+
             <div className="h-135 md:min-h-screen md:sticky top-0 overflow-hidden z-0">
                 <AuthorityHero />
             </div>
 
-            {/* OVERLAY CONTENT 
-                relative z-10 ensures this layer is on top.
-                The -mt-20 pull-up creates a seamless transition over the Hero's bottom edge.
-                rounded-t-[40px] maintains the premium aesthetic.
-            */}
             <div className="
-  relative z-10 bg-[#FAFAFA]
+  relative z-10 
   rounded-t-[40px] md:rounded-t-[80px]
   shadow-[0_-30px_60px_rgba(0,0,0,0.4)]
   -mt-20 sm:-mt-20
@@ -32,7 +24,7 @@ const ShopPage = () => {
 
                 <ShopComponent />
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
