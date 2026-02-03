b9dcc92ddec8c9e810da07c6a525bcbda04cb141
afterrender1 <afterrender1@gmail.com>
2026-02-03 16:43:13 +0500

    Update ProductImageGallery.jsx

--- app/components/ProductImageGallery.jsx
+++ app/components/ProductImageGallery.jsx
@@ -36,10 +36,10 @@ export default function ProductImageGallery({ src, alt, discount }) {
     return (
         <>
             <div
-                className="group relative aspect-square  rounded-lg md:rounded-lg  overflow-hidden cursor-zoom-in"
+                className="group relative aspect-square   overflow-hidden cursor-zoom-in"
                 onClick={() => setIsFullScreen(true)}
             >
-                <div className="absolute inset-0 z-10 bg-linear-to-tr from-white/20 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-700" />
+                {/* <div className="absolute inset-0 z-10 bg-linear-to-tr from-white/20 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-700" /> */}
 
                 <Image
                     src={src}
@@ -50,7 +50,7 @@ export default function ProductImageGallery({ src, alt, discount }) {
                     className="object-cover p-8 md:p-16 transition-transform duration-1000 group-hover:scale-105"
                 />
 
-                <div className="absolute inset-0 bg-slate-900/5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+                <div className="absolute inset-0 bg-slate-900/5 opacity-0  transition-opacity duration-300 flex items-center justify-center">
                     <div className="bg-white/90 backdrop-blur-md p-4 rounded-full shadow-2xl scale-50 group-hover:scale-100 transition-transform duration-500">
                         <ZoomIn size={24} className="text-slate-900" />
                     </div>
@@ -67,11 +67,11 @@ export default function ProductImageGallery({ src, alt, discount }) {
             {/* Fullscreen Modal */}
             {isFullScreen && (
                 <div
-                    className="fixed inset-0 z-50 bg-black/90 flex items-center justify-center p-4 md:p-12"
+                    className="fixed inset-0 z-50 bg-white flex items-center justify-center p-4 md:p-12"
                     onClick={() => setIsFullScreen(false)}
                 >
                     <button
-                        className="absolute top-6 right-6 md:top-12 md:right-12 p-2 text-white border border-gray-700 backdrop-blur-md rounded-lg z-50"
+                        className="absolute top-6 right-6 md:top-12 md:right-12 p-2 text-black border border-gray-700 backdrop-blur-md rounded-lg z-50"
                         onClick={(e) => { e.stopPropagation(); setIsFullScreen(false); }}
                     >
                         <X size={24} />
@@ -83,7 +83,7 @@ export default function ProductImageGallery({ src, alt, discount }) {
                             alt={alt}
                             width={1200}
                             height={1200}
-                            className="object-contain max-h-screen max-w-full rounded-lg shadow-2xl"
+                            className="object-contain max-h-screen max-w-full "
                         />
                     </div>
                 </div>
